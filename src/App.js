9f808f9730fcc64788d7895e3071943cd3baa7df
Victor Ribeiro <ribeirovictorc@gmail.com>
2021-07-15 21:27:49 -0300

    simple autologin

--- src/App.js
+++ src/App.js
@@ -34,22 +34,26 @@ const App = () => {
   const { data: user, loading } = useSelector((state) => state.user);
 
   useEffect(() => {
-    dispatch(autoLogin())
-      .then(() => {
-        if (user?.user?.is_mentor || user?.user?.is_judge) {
-          window.location.pathname.split("/")[1] === "auth" &&
-            history.push("/meus-desafios");
-        } else {
-          window.location.pathname.split("/")[1] === "auth" &&
-            history.push("/dashboard");
-        }
-      })
-      .catch((err) => {
-        // console.log(err);
-        dispatch(UserActions.logoutSuccess());
-        // history.push("/auth/mentor/login");
-      });
+    dispatch(autoLogin());
   }, [dispatch]);
+
+  // useEffect(() => {
+  //   dispatch(autoLogin())
+  //     .then(() => {
+  //       if (user?.user?.is_mentor || user?.user?.is_judge) {
+  //         window.location.pathname.split("/")[1] === "auth" &&
+  //           history.push("/meus-desafios");
+  //       } else {
+  //         window.location.pathname.split("/")[1] === "auth" &&
+  //           history.push("/dashboard");
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       // console.log(err);
+  //       dispatch(UserActions.logoutSuccess());
+  //       // history.push("/auth/mentor/login");
+  //     });
+  // }, [dispatch]);
 
   // useEffect(() => {
   //   dispatch(autoLogin());

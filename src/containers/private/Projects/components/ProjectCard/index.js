95b3e4ed611eb612f30999dcfad9418c9fb031d7
sandrofilhoo <67797843+sandrofilhoo@users.noreply.github.com>
2021-09-20 08:35:40 -0300

    change in substring

--- src/containers/private/Projects/components/ProjectCard/index.js
+++ src/containers/private/Projects/components/ProjectCard/index.js
@@ -1,37 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link } from "react-router-dom"
 
-import { Card } from "components/Card";
-import { Title, Text } from "components/Text";
+import { Card } from "components/Card"
+import { Title, Text } from "components/Text"
 
-import styles from "./styles.module.sass";
-import defaultImage from "assets/components/MainImage/image.png";
+import styles from "./styles.module.sass"
+import defaultImage from "assets/components/MainImage/image.png"
 
-import { BASEURL } from "utils/api";
+import { BASEURL } from "utils/api"
 
 const ProjectCard = (props) => {
-  const { data } = props;
-  return (
-    <Card border noShadow className={styles.card}>
-      <Link to={props.to} style={{ textDecoration: "none" }}>
-        <header className={styles.header}>
-          <Text size={18} color="white">
-            {data?.name || "..."}
-          </Text>
-        </header>
-        <div className={styles.image__header}>
-          <img
-            src={!!data?.file?.length ? BASEURL + data?.file : defaultImage}
-            alt={data?.name}
-          />
-        </div>
-        <section className={styles.content}>
-          <Title>{data?.name || "..."}</Title>
-          <Text>{data?.description.substring(0, 200) + "..." || "..."}</Text>
-        </section>
-      </Link>
-    </Card>
-  );
-};
+	const { data } = props
+	return (
+		<Card border noShadow className={styles.card}>
+			<Link to={props.to} style={{ textDecoration: "none" }}>
+				<header className={styles.header}>
+					<Text size={18} color="white">
+						{data?.name || "..."}
+					</Text>
+				</header>
+				<div className={styles.image__header}>
+					<img
+						src={!!data?.file?.length ? BASEURL + data?.file : defaultImage}
+						alt={data?.name}
+					/>
+				</div>
+				{console.log("erro aqui", data)}
+				<section className={styles.content}>
+					<Title>{data?.name || "..."}</Title>
+					<Text>
+						{data && data?.description
+							? data?.description.substring(0, 200) + "..."
+							: "..."}
+					</Text>
+				</section>
+			</Link>
+		</Card>
+	)
+}
 
-export { ProjectCard };
+export { ProjectCard }
